43df80cfa359f0af9d52118cb9180c7ad482b24a
Jason Lau <39978937+khlauaj@users.noreply.github.com>
2023-05-24 00:03:30 +0800

    Add files via upload

---
+++ scratch-pad/app.js
@@ -0,0 +1,24 @@
+const API_KEY = "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"
+
+async function fetchData(){
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: "Hello!"
+                }
+            ]
+        })
+    })
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchData()
